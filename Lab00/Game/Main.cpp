dad48859d453619867775ead65156bc000853a48
DESKTOP-0FNNCB7\jinju <alegruz@khu.ac.kr>
2022-03-03 15:45:30 +0900

    [FIX] added namespace to PrintHi in Main.cpp

--- Lab00/Game/Main.cpp
+++ Lab00/Game/Main.cpp
@@ -36,7 +36,7 @@
 -----------------------------------------------------------------F-F*/
 INT WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ INT nCmdShow)
 {
-  PrintHi();
+  library::PrintHi();
 
   return static_cast<INT>(msg.wParam);
 }
