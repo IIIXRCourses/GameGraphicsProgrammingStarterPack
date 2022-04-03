b74114ae995a775b492b93cffca1c1eb2a1e1356
DESKTOP-0FNNCB7\jinju <alegruz@khu.ac.kr>
2022-04-03 17:52:49 +0900

    [ADD] added pragma comments

--- Lab04/Library/Common.h
+++ Lab04/Library/Common.h
@@ -23,6 +23,9 @@
 #include <wincodec.h>
 #include <wrl.h>
 
+#pragma comment(lib, "d3d11.lib")
+#pragma comment(lib, "d3dcompiler.lib")
+
 #include <d3d11_4.h>
 #include <d3dcompiler.h>
 #include <directxcolors.h>
