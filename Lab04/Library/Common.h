7bb6354e64bac4205e2b532bca2c37247dfd22ad
DESKTOP-0FNNCB7\jinju <alegruz@khu.ac.kr>
2022-04-03 17:48:09 +0900

    [ADD] added WIN32_LEAN_AND_MEAN macro

--- Lab04/Library/Common.h
+++ Lab04/Library/Common.h
@@ -15,6 +15,10 @@
 #define UNICODE
 #endif // ! UNICODE
 
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif // ! WIN32_LEAN_AND_MEAN
+
 #include <windows.h>
 #include <wincodec.h>
 #include <wrl.h>
