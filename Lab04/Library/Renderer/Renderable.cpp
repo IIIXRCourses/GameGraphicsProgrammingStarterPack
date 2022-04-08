eefdaaadf52d9dd69ab73a417551197d14c1e0f7
DESKTOP-0FNNCB7\jinju <alegruz@khu.ac.kr>
2022-04-08 20:52:59 +0900

    [ADD] added transformation methods

--- Lab04/Library/Renderer/Renderable.cpp
+++ Lab04/Library/Renderer/Renderable.cpp
@@ -135,4 +135,102 @@ namespace library
     /*--------------------------------------------------------------------
       TODO: Renderable::GetWorldMatrix definition (remove the comment)
     --------------------------------------------------------------------*/
+
+    /*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
+      Method:   Renderable::RotateX
+
+      Summary:  Rotates around the x-axis
+
+      Args:     FLOAT angle
+                  Angle of rotation around the x-axis, in radians
+
+      Modifies: [m_world].
+    M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
+    void Renderable::RotateX(_In_ FLOAT angle)
+    {
+        // m_world *= x-axis rotation by angle matrix
+    }
+
+    /*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
+      Method:   Renderable::RotateY
+
+      Summary:  Rotates around the y-axis
+
+      Args:     FLOAT angle
+                  Angle of rotation around the y-axis, in radians
+
+      Modifies: [m_world].
+    M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
+    void Renderable::RotateY(_In_ FLOAT angle)
+    {
+        // m_world *= y-axis rotation by angle matrix
+    }
+
+    /*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
+      Method:   Renderable::RotateZ
+
+      Summary:  Rotates around the z-axis
+
+      Args:     FLOAT angle
+                  Angle of rotation around the z-axis, in radians
+
+      Modifies: [m_world].
+    M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
+    void Renderable::RotateZ(_In_ FLOAT angle)
+    {
+        // m_world *= z-axis rotation by angle matrix
+    }
+
+    /*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
+      Method:   Renderable::RotateRollPitchYaw
+
+      Summary:  Rotates based on a given pitch, yaw, and roll (Euler angles)
+
+      Args:     FLOAT pitch
+                  Angle of rotation around the x-axis, in radians
+                FLOAT yaw
+                  Angle of rotation around the y-axis, in radians
+                FLOAT roll
+                  Angle of rotation around the z-axis, in radians
+
+      Modifies: [m_world].
+    M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
+    void Renderable::RotateRollPitchYaw(_In_ FLOAT pitch, _In_ FLOAT yaw, _In_ FLOAT roll)
+    {
+        // m_world *= x, y, z-axis rotation by pitch, yaw, roll matrix
+    }
+
+    /*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
+      Method:   Renderable::Scale
+
+      Summary:  Scales along the x-axis, y-axis, and z-axis
+
+      Args:     FLOAT scaleX
+                  Scaling factor along the x-axis.
+                FLOAT scaleY
+                  Scaling factor along the y-axis.
+                FLOAT scaleZ
+                  Scaling factor along the z-axis.
+
+      Modifies: [m_world].
+    M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
+    void Renderable::Scale(_In_ FLOAT scaleX, _In_ FLOAT scaleY, _In_ FLOAT scaleZ)
+    {
+        // m_world *= x, y, z-axis scaling by scale factor matrix
+    }
+
+    /*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
+      Method:   Renderable::Translate
+
+      Summary:  Translates matrix from a vector
+
+      Args:     const XMVECTOR& offset
+                  3D vector describing the translations along the x-axis, y-axis, and z-axis
+
+      Modifies: [m_world].
+    M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
+    void Renderable::Translate(_In_ const XMVECTOR& offset)
+    {
+        // m_world *= translate by offset vector matrix
+    }
 }
--- Lab04/Library/Renderer/Renderable.h
+++ Lab04/Library/Renderer/Renderable.h
@@ -72,6 +72,13 @@ namespace library
         ComPtr<ID3D11Buffer>& GetConstantBuffer();
         const XMMATRIX& GetWorldMatrix() const;
 
+        void RotateX(_In_ FLOAT angle);
+        void RotateY(_In_ FLOAT angle);
+        void RotateZ(_In_ FLOAT angle);
+        void RotateRollPitchYaw(_In_ FLOAT roll, _In_ FLOAT pitch, _In_ FLOAT yaw);
+        void Scale(_In_ FLOAT scaleX, _In_ FLOAT scaleY, _In_ FLOAT scaleZ);
+        void Translate(_In_ const XMVECTOR& offset);
+
         virtual UINT GetNumVertices() const = 0;
         virtual UINT GetNumIndices() const = 0;
     protected:
