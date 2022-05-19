437ad6fe2dc5ef9d55ecd6e36fb974d0558e47c4
Bora Jeong <jbr99@naver.com>
2022-05-19 21:34:33 +0900

    [LAB08] FIX Renderer.h

--- Lab08/Library/Renderer/Renderer.h
+++ Lab08/Library/Renderer/Renderer.h
@@ -69,7 +69,7 @@ namespace library
 
         void HandleInput(_In_ const DirectionsInput& directions, _In_ const MouseRelativeMovement& mouseRelativeMovement, _In_ FLOAT deltaTime);
         void Update(_In_ FLOAT deltaTime);
-        void Render(UINT boneIdex);
+        void Render();
 
         HRESULT SetVertexShaderOfRenderable(_In_ PCWSTR pszRenderableName, _In_ PCWSTR pszVertexShaderName);
         HRESULT SetPixelShaderOfRenderable(_In_ PCWSTR pszRenderableName, _In_ PCWSTR pszPixelShaderName);
